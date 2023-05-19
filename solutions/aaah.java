223d26c9a2d835a0e0c560b0c3e296768826a81c
moltenpanther <105462919+moltenpanther@users.noreply.github.com>
2023-05-18 20:12:07 -0500

    Create aaah.java

---
+++ solutions/aaah.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Kattis {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String ah1 = scan.next();
+        String ah2 = scan.next();
+
+        if (ah1.length() >= ah2.length()) {
+            System.out.println("go");
+        } else {
+            System.out.println("no");
+        }
+
+        scan.close();
+    }
+
+}
