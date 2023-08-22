e549769637a782909a1b88b1d6c61f1b1bc6db4d
moltenpanther <105462919+moltenpanther@users.noreply.github.com>
2023-08-22 13:13:04 -0500

    Create cinema.java

---
+++ solutions/cinema.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class cinema {
+
+    public static void main(String[] args) {
+        
+    	Scanner scan = new Scanner(System.in);
+    	
+    	int seats = scan.nextInt();
+    	int groups = scan.nextInt();
+    	
+    	int left = groups;
+    	for(int i = 0; i < groups; i++) {
+    		
+    		int num = scan.nextInt();
+    		if(num <= seats) {
+    			seats -= num;
+    			left--;
+    		}
+    		
+    	}
+    	
+    	System.out.println(left);
+    	
+    	scan.close();
+    	
+    }
+
+}
