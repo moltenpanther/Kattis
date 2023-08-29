af7987bf1e40d7d6b8f55cde093b602d863a6359
moltenpanther <105462919+moltenpanther@users.noreply.github.com>
2023-08-29 13:05:03 -0500

    Create anotherbrick.java

---
+++ solutions/anotherbrick.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class anotherbrick {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        
+        int height = scan.nextInt();
+        int width = scan.nextInt();
+        int bricks = scan.nextInt();
+        
+        /*int[] brick = new int[bricks];
+        for(int i = 0; i < bricks; i++) {
+        	brick[i] = scan.nextInt();
+        }
+        */
+        
+        // Counting
+        int sum = 0;
+        int layers = 0;
+        boolean good = false;
+        for(int i = 0; i < bricks; i++) {
+        	
+        	int brick = scan.nextInt();
+        	sum += brick;
+        	
+        	// Fits
+        	if(sum == width) {
+        		layers++;
+        		if(layers == height) {
+        			i = bricks;
+        			good = true;
+        		}
+        		sum = 0;
+        	}
+        	
+        	// Too big
+        	if(sum > width) {
+        		i = bricks;
+        	}        	
+        }
+        
+        if(good) {
+        	System.out.println("YES");
+        }else {
+        	System.out.println("NO");
+        }
+                
+        scan.close();
+    }
+
+}
