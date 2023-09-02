3c1be41883288713366b742a1546eb09478cae94
moltenpanther <105462919+moltenpanther@users.noreply.github.com>
2023-09-02 11:34:06 -0500

    Update spritt.java

--- solutions/spritt.java
+++ solutions/spritt.java
@@ -1,18 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class spritt {
 
-    public static void main(String[] args) {
-
-    	Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws NumberFormatException, IOException {
     	
-    	int classes = scan.nextInt();
-    	int bottles = scan.nextInt();
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	String[] input = br.readLine().split(" ");  	
+    	int classes = Integer.parseInt(input[0]);
+    	int bottles = Integer.parseInt(input[1]);
     	
     	boolean enough = true;
     	for(int i = 0; i < classes; i++) {
     		   
-    		int num = scan.nextInt();
+    		int num = Integer.parseInt(br.readLine());
     		bottles -= num;
     		
     		if(bottles < 0) {
@@ -27,7 +31,8 @@ public class spritt {
     		System.out.println("Neibb");
     	}
     	
-    	scan.close();
+    	br.close();
+    	
     }
 
 }
