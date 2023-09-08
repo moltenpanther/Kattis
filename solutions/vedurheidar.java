861da15428239699501cc6464f4cea17db9da385
moltenpanther <105462919+moltenpanther@users.noreply.github.com>
2023-09-07 22:30:15 -0500

    Create vedurheidar.java

---
+++ solutions/vedurheidar.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class vedurheidar {
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	Scanner scan = new Scanner(System.in);
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	    	
+    	int num = scan.nextInt();
+    	for(int i = 0; i < num; i++) {
+	    	
+    	}
+    	
+    	// Output
+    	System.out.println("");
+   		
+    	br.close();
+    	scan.close();
+    }
+
+}
