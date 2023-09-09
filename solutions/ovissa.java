e260a0f2a6ac909eeb9c6e2ac6bcd23b8338bcc2
moltenpanther <105462919+moltenpanther@users.noreply.github.com>
2023-09-08 22:57:41 -0500

    Create ovissa.java

---
+++ solutions/ovissa.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ovissa {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        String count = scan.next();
+        System.out.println(count.length());
+
+    }
+
+}
+
