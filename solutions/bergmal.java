a180fb7773988f459b52621b1ec5b97947574b16
Ben Cobb <105462919+moltenpanther@users.noreply.github.com>
2024-09-12 10:38:16 -0500

    Create bergmal.java

---
+++ solutions/bergmal.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bergmal {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // Output
+        System.out.println(br.readLine());
+
+        br.close();
+    }
+
+}
