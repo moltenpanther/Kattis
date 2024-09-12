b69a61859156b18bad1dc1b6e895e068b8902eb6
Ben Cobb <105462919+moltenpanther@users.noreply.github.com>
2024-09-12 10:22:58 -0500

    Create hipphipp.java

---
+++ solutions/hipphipp.java
@@ -0,0 +1,16 @@
+public class hipphipp {
+	
+    public static void main(String[] args) {
+    	
+    	StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < 20; i++) {
+        	sb.append("Hipp hipp hurra!\n");
+        }
+        
+        // Output
+    	System.out.println(sb);
+   		
+    }
+
+}
