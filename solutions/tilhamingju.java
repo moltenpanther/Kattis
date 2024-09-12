7c23b1f764d5706428ea7adaec7fae641bca305d
Ben Cobb <105462919+moltenpanther@users.noreply.github.com>
2024-09-12 10:25:02 -0500

    Create tilhamingju.java

---
+++ solutions/tilhamingju.java
@@ -0,0 +1,10 @@
+public class tilhamingju {
+	
+    public static void main(String[] args) {
+    	
+        // Output
+    	System.out.println("TIL HAMINGJU MED AFMAELID FORRITUNARKEPPNI FRAMHALDSSKOLANNA!");
+   		
+    }
+
+}
